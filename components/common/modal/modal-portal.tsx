95412ae7d5a09720371baa66aac5219aee09a3df
hanbin <rj2303@naver.com>
2024-05-28 23:29:53 +0900

    refactor: 포탈 요소 취득 시 useEffect 제거

--- components/common/modal/modal-portal.tsx
+++ components/common/modal/modal-portal.tsx
@@ -1,27 +1,27 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Modal, { CommonProps } from './modal';
 import styles from './modal-portal.module.scss';
 import { createPortal } from 'react-dom';
 
+export type Buttons = {
+  text: string;
+  event: () => void;
+}[];
+
 interface ModalPortalProps extends CommonProps {
-  buttons: {
-    text: string;
-    event: () => void;
-  }[];
+  buttons: Buttons;
 }
 
 export default function ModalPortal({ message, buttons, handleClose }: ModalPortalProps) {
   const [portalRef, setPortalRef] = useState<HTMLElement | null>(null);
+
+  if (!portalRef) setPortalRef(document.getElementById('__container'));
 
   const buttonComps = buttons.map((button, i) => (
     <button className={`${styles.button} ${i === buttons.length - 1 ? styles.greenButton : ''}`} onClick={button.event}>
       {button.text}
     </button>
   ));
-
-  useEffect(() => {
-    setPortalRef(document.getElementById('__container'));
-  }, []);
 
   return (
     <>
