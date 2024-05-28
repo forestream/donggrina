9087fad38888781ac1a07d6f29a816cc2555f835
mun-jihye <m84422583@gmail.com>
2024-05-28 21:46:41 +0900

    feat: left arrow 클릭 시 이전 화면으로 이동 로직 구현

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,10 +1,16 @@
 import Image from 'next/image';
 import styles from './Header.module.scss';
+import { useRouter } from 'next/router';
 
 export function Header() {
+  const router = useRouter();
+
+  const handleClick = () => {
+    router.back();
+  };
   return (
     <header className={styles.header}>
-      <button className={styles.leftArrow}>
+      <button className={styles.leftArrow} onClick={handleClick}>
         <Image src="/images/header/left-arrow.png" alt="뒤로 가기" fill />
       </button>
       <div className={styles.imageBox}>
