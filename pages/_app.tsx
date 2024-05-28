1fac3f2b5eeebf62a1556a252e77e442c8d036fd
mun-jihye <m84422583@gmail.com>
2024-05-28 21:51:17 +0900

    [#22] feat: login, landing, 404페이지가 아닐 경우에 해더가 보임

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,17 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { Header } from '@/components/common/Header';
+import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  const hide = router.pathname === '/login' || router.pathname === '/landing' || router.pathname === '/404';
+
   return (
     <div id="__wrap">
       <main id="__container">
-        <Header />
+        {!hide && <Header />}
         <Component {...pageProps} />
       </main>
     </div>
