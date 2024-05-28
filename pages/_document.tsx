652decf89dba37461d63aaec466f7981f662fead
LimDong <ilhai6@naver.com>
2024-05-28 13:53:55 +0900

    [#19] fix: 공통 버튼 스타일 분할 및 subset 폰트로 변경

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -2,14 +2,12 @@ import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="ko">
       <Head>
         <link
           rel="stylesheet"
-          href="https://cdnjs.cloudflare.com/ajax/libs/pretendard/1.3.9/static/pretendard.css"
-          integrity="sha512-NzqTHTrO48HsIamogmIaVhTXoSgRF24Cn+ynrNYrFuKrY0AdDbmcNieiOHsQARS/r0Gax9VwV3/rVMHs3ipUlg=="
-          crossOrigin="anonymous"
-          referrerPolicy="no-referrer"
+          as="style"
+          href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard-dynamic-subset.min.css"
         />
       </Head>
       <body>
