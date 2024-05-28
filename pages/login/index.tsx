2ad46d71d18dcd95eb7b165446eb30591b878436
LimDong <ilhai6@naver.com>
2024-05-28 12:02:56 +0900

    [#19] feat: 로그인 페이지 퍼블 완료 및 버튼 공통 스타일링 완료

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,10 +1,12 @@
-import styles from './index.module.css';
+import styles from './index.module.scss';
 import LoginTop from '@/components/login/loginTop/loginTop';
+import LoginButtons from '@/components/login/loginBottom/loginButtons';
 
 export default function Login() {
   return (
     <article className={styles.article}>
       <LoginTop />
+      <LoginButtons />
     </article>
   );
 }
