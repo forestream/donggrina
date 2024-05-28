c32081fbb07723966b4a919e85d7bd05ef45c23c
mun-jihye <m84422583@gmail.com>
2024-05-28 17:39:29 +0900

    design: header layout 잡기

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,12 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
+import { Header } from '@/components/common/Header';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div id="__wrap">
       <main id="__container">
+        <Header />
         <Component {...pageProps} />
       </main>
     </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@
 export default function Home() {
   return (
     <>
-      <div>안녕</div>
+      <div style={{ paddingTop: '54px' }}>안녕</div>
     </>
   );
 }
