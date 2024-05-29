ce7f01a1e84261f2a771df46e76750d03be92e3a
mun-jihye <m84422583@gmail.com>
2024-05-29 17:00:29 +0900

    [#22] refactor: 헤더를 안보이게 할 요소를 유틸 함수로 분리

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,16 @@
 import type { AppProps } from 'next/app';
 import '@/styles/globals.css';
 import { Header } from '@/components/common/Header';
+import { isHeader } from '@/utils/IsHeader';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
 
-  const hide = router.pathname === '/login' || router.pathname === '/landing' || router.pathname === '/404';
-
   return (
     <div id="__wrap">
       <main id="__container">
-        {!hide && <Header />}
+        {!isHeader(router) && <Header />}
         <Component {...pageProps} />
       </main>
     </div>
