60d7ed497af0de1b773bc8d7c5def8179abd665a
hanbin <rj2303@naver.com>
2024-05-29 14:28:37 +0900

    docs: useModal 훅에 jsdoc 작성

--- hooks/use-modal.tsx
+++ hooks/use-modal.tsx
@@ -1,6 +1,12 @@
 import ModalPortal, { Buttons } from '@/components/common/modal/modal-portal';
 import { Dispatch, ReactNode, SetStateAction, useState } from 'react';
 
+/**
+ * 모달 메세지와 버튼 텍스트, 이벤트 핸들러 배열을 전달하고 Modal 컴포넌트와 setIsOpen 함수로 모달을 열고 닫으세요.
+ * @param message 모달에 전달할 메세지.
+ * @param buttons 모달 버튼의 글자와 이벤트 핸들러.
+ * @returns {{Modal: Function, setIsOpen: function(boolean)}[]} - setIsOpen에 불리언 값을 전달해서 Modal 컴포넌트를 열고 닫으세요.
+ */
 export default function useModal(
   message: string,
   buttons: Buttons,
