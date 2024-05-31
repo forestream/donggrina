1fe2980da2710aa66160c0ff1c77bf3bfa5d4b7c
Lim dong hyeon <ilhai3@naver.com>
2024-06-01 01:06:17 +0900

    [#19] Feat: 로그인 기능 추가 및 환경 변수 설정

--- components/login/kakao-button/kakao-button.tsx
+++ components/login/kakao-button/kakao-button.tsx
@@ -1,14 +1,17 @@
-import Link from 'next/link';
 import styles from './kakao-button.module.scss';
 import KakaoSVG from '@/public/images/login/kakao_logo.svg';
 
 export default function KakaoButton() {
-  const kakaoURL = `/oauth2/authorization/kakao`;
+  const kakaoURL = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`;
+
+  const handleLogin = () => {
+    window.location.href = kakaoURL;
+  };
 
   return (
-    <Link className={styles.kakaoButton} href={kakaoURL}>
+    <button className={styles.kakaoButton} onClick={handleLogin} type="button">
       <KakaoSVG />
       <span>카카오로 로그인</span>
-    </Link>
+    </button>
   );
 }
