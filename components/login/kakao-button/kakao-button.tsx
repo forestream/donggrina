eed3f85153ef41f71df588d17ea04de67f4e9555
LimDong <ilhai6@naver.com>
2024-06-01 18:20:01 +0900

    [#19] Fix: config 폴더로 환경변수 관리 및 카카오 로그인 태그 변경

--- components/login/kakao-button/kakao-button.tsx
+++ components/login/kakao-button/kakao-button.tsx
@@ -1,17 +1,15 @@
+import Link from 'next/link';
 import styles from './kakao-button.module.scss';
 import KakaoSVG from '@/public/images/login/kakao_logo.svg';
+import { config } from '@/config';
 
 export default function KakaoButton() {
-  const kakaoURL = `${process.env.NEXT_PUBLIC_API_URL}/oauth2/authorization/kakao`;
-
-  const handleLogin = () => {
-    window.location.href = kakaoURL;
-  };
+  const kakaoURL = config.kakaoAuth;
 
   return (
-    <button className={styles.kakaoButton} onClick={handleLogin} type="button">
+    <Link className={styles.kakaoButton} href={kakaoURL}>
       <KakaoSVG />
       <span>카카오로 로그인</span>
-    </button>
+    </Link>
   );
 }
