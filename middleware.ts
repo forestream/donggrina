03e1efaa1aca92264e38b014ce52218e928607c4
Lim dong hyeon <ilhai3@naver.com>
2024-06-03 16:54:23 +0900

    [#39] Fix: middleware 코드 수정

--- middleware.ts
+++ middleware.ts
@@ -11,22 +11,7 @@ export default function middleware(request: NextRequest) {
   const isPublicPage = PUBLIC_PAGES.includes(path);
 
   // 로그인 상태 확인
-  const hasCookie = cookies.has('accessToken');
-
-  // 쿼리 파라미터에서 token 값 확인
-  const hasQuery = nextUrl.search.includes('token');
-
-  // /start-family 페이지로 접근하는데 token이 있을 경우 : 페이지 접근
-  // /start-family 페이지로 접근하는데 token이 없을 경우 : 리다이렉션
-  // /start-family 페이지로 접근하는데 token이 없지만 쿠키가 있을 경우 : 페이지 접근
-  // 추후 배포됐을 때 수정.
-  if (path === '/start-family') {
-    if (hasQuery || hasCookie) {
-      return NextResponse.next();
-    } else {
-      return NextResponse.redirect(new URL('/login', request.nextUrl));
-    }
-  }
+  const hasCookie = cookies.has('Authorization');
 
   // 비로그인 상태일 때 로그인이 필요한 페이지에 접근 시 리다이렉션
   if (!hasCookie && isProtectedPage) {
