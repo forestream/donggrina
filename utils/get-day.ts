167f1a317c93c94899771378e024b7cf1abfc56c
LimDong <ilhai6@naver.com>
2024-06-05 18:31:47 +0900

    [#49] Fix: pull 받았던 calendar 컴포넌트 제거

--- utils/get-day.ts
+++
@@ -1,7 +0,0 @@
-import { CALENDAR_DAYS_KOREAN } from '@/lib/constants/calendar-constants';
-
-export default function getDay(year: string, month: string, date: string) {
-  const dateObj = new Date(+year, +month, +date);
-  const day = dateObj.getDay();
-  return CALENDAR_DAYS_KOREAN[day];
-}
--- utils/get-first-day.ts
+++
@@ -1,4 +0,0 @@
-export default function getFirstDay(year: number, month: number) {
-  const firstDay = new Date(year, month - 1).getDay();
-  return firstDay;
-}
