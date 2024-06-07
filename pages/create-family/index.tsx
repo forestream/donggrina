10d67034e6f14124d1b06c83610dcfe0fc04b8ff
LimDong <ilhai6@naver.com>
2024-06-07 14:14:56 +0900

    [#63] Rename: 파일 임시 저장

--- pages/create-family/index.tsx
+++ pages/create-family/index.tsx
@@ -1,3 +1,3 @@
 export default function CreateFamily() {
-  return <section style={{ padding: '126px 24px 0 24px' }}>asd</section>;
+  return <section style={{ padding: '126px 24px 0 24px' }}></section>;
 }
