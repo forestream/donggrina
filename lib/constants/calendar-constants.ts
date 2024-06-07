043b58dce4f0f0f533d10ab0bddb7e7475a2f4d6
hanbin <rj2303@naver.com>
2024-06-07 13:07:22 +0900

    [#44] Design: 달력 요일 한글로 변경

--- lib/constants/calendar-constants.ts
+++ lib/constants/calendar-constants.ts
@@ -1,4 +1,4 @@
-export const CALENDAR_DAYS = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
+export const CALENDAR_DAYS = ['월', '화', '수', '목', '금', '토', '일'];
 
 export const CALENDAR_EMPTY_DATES = [6, 0, 1, 2, 3, 4, 5];
 
