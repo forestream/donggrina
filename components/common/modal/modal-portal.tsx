667888f87d0f225c061e0583abc9d08330fe4bfe
hanbin <rj2303@naver.com>
2024-06-07 16:50:10 +0900

    [#23] feat: 공용 모달 코드 수정
    기존에 모달에 띄울 메세지와 버튼을 프롭으로 전달받는 대신,
    모달 껍데기만 남겨두고 컴포넌트를 받도록 수정했습니다.
    추가 수정될 수 있음

--- components/common/modal/modal-portal.tsx
+++ components/common/modal/modal-portal.tsx
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import Modal, { ModalCommonProps } from './modal';
-import styles from './modal-portal.module.scss';
 import { createPortal } from 'react-dom';
 
 export type Buttons = {
@@ -8,25 +7,12 @@ export type Buttons = {
   event: () => void;
 }[];
 
-interface ModalPortalProps extends ModalCommonProps {
-  buttons: Buttons;
-}
-
-export default function ModalPortal({ message, buttons, onClose }: ModalPortalProps) {
+export default function ModalPortal({ content, onClose }: ModalCommonProps) {
   const [portalElement, setPortalElement] = useState<HTMLElement | null>(null);
 
   if (!portalElement) setPortalElement(document.getElementById('__container'));
 
-  const buttonComps = buttons.map((button, i) => (
-    <button className={`${styles.button} ${i === buttons.length - 1 ? styles.greenButton : ''}`} onClick={button.event}>
-      {button.text}
-    </button>
-  ));
-
   return (
-    <>
-      {portalElement &&
-        createPortal(<Modal message={message} buttons={buttonComps} onClose={onClose} />, portalElement as HTMLElement)}
-    </>
+    <>{portalElement && createPortal(<Modal content={content} onClose={onClose} />, portalElement as HTMLElement)}</>
   );
 }
--- hooks/use-modal.tsx
+++ hooks/use-modal.tsx
@@ -1,23 +1,24 @@
-import ModalPortal, { Buttons } from '@/components/common/modal/modal-portal';
-import { Dispatch, ReactNode, SetStateAction, useState } from 'react';
+import ModalPortal from '@/components/common/modal/modal-portal';
+import { ReactNode, useState } from 'react';
 
 /**
  * 모달 메세지와 버튼 텍스트, 이벤트 핸들러 배열을 전달하고 Modal 컴포넌트와 setIsOpen 함수로 모달을 열고 닫으세요.
  * @param {string} message - 모달에 전달할 메세지.
- * @param {{text: string, event: Function}[]} buttons - 모달 버튼 글자(text)와 이벤트 핸들러 배열(event)을 포함하는 객체.
- * @returns {{Modal: Function, setIsOpen: function(boolean)}[]} - setIsOpen에 불리언 값을 전달해서 Modal 컴포넌트를 열고 닫으세요.
+ * @param {{text: string, event: Function}[]} buttons - 모달 버튼 글자(text)와 이벤트 핸들러(event)를 포함하는 객체의 배열.
+ * @returns {[Modal: Function, handleModal: function(boolean)]} - setIsOpen에 불리언 값을 전달해서 Modal 컴포넌트를 열고 닫으세요.
  */
-export default function useModal(
-  message: string,
-  buttons: Buttons,
-): [() => ReactNode, Dispatch<SetStateAction<boolean>>] {
+export default function useModal(content: ReactNode): [() => ReactNode, (isOpen: boolean) => void] {
   const [isOpen, setIsOpen] = useState(false);
+
+  const handleModal = (isOpen: boolean) => {
+    setIsOpen(isOpen);
+  };
 
   const handleClose = () => {
     setIsOpen(false);
   };
 
-  const Modal = () => <>{isOpen && <ModalPortal message={message} buttons={buttons} onClose={handleClose} />}</>;
+  const Modal = () => <>{isOpen && <ModalPortal content={content} onClose={handleClose} />}</>;
 
-  return [Modal, setIsOpen];
+  return [Modal, handleModal];
 }
