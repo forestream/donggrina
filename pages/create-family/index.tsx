6d599945781f766c52c2fc4a859c797e046babb7
LimDong <ilhai6@naver.com>
2024-06-07 19:30:03 +0900

    [#63] Feat: 가족 만들기 페이지 시작

--- pages/create-family/index.tsx
+++ pages/create-family/index.tsx
@@ -1,3 +1,13 @@
+import Title from '@/components/common/title/title';
+import SubTitle from '@/components/common/title/sub-title/sub-title';
+
 export default function CreateFamily() {
-  return <section style={{ padding: '126px 24px 0 24px' }}></section>;
+  return (
+    <section style={{ padding: '126px 24px 0 24px' }}>
+      <div>
+        <SubTitle>가족등록하기</SubTitle>
+        <Title>가족신청서</Title>
+      </div>
+    </section>
+  );
 }
