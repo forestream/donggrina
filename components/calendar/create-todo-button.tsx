e8ebbfc5b12025a2c250297dcd0625c5d3b3ff1f
hanbin <rj2303@naver.com>
2024-06-10 19:01:38 +0900

    [#44] Feat: 일정 추가 버튼 수정
    calendarContext 에서 연월일 참조하도록 수정.
    아이콘 수정.

--- components/calendar/create-todo-button.tsx
+++ components/calendar/create-todo-button.tsx
@@ -2,11 +2,16 @@ import Image from 'next/image';
 import styles from './create-todo-button.module.scss';
 import Link from 'next/link';
 import { CalendarProps } from '@/pages/calendar';
+import { useCalendarContext } from '../calendar-compound/calendar';
 
-export default function CreateTodoButton({ year, month, date }: CalendarProps) {
+export default function CreateTodoButton({ year }: CalendarProps) {
+  const calendarContext = useCalendarContext();
+  const month = calendarContext.month + 1;
+  const date = calendarContext.date;
+
   return (
     <Link href={`/calendar/create?year=${year}&month=${month}&date=${date}`} className={styles.outer}>
-      <Image src="images/calendar/new-todo.svg" alt="할일 추가 버튼" width={30} height={30} />
+      <Image src="images/calendar/new-todo.svg" alt="할일 추가 버튼" width={20} height={20} />
     </Link>
   );
 }
