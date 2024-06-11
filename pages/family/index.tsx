7fa1455f5c11be71abed6b1e5cce57d30436d032
d5ng <dong5ffice@gmail.com>
2024-06-11 12:57:51 +0900

    [#79] Design: 오늘 날짜 UI 추가

--- pages/family/index.tsx
+++ pages/family/index.tsx
@@ -1,3 +1,20 @@
+import CalendarInstance from '@/utils/date/date.utils';
+import styles from './index.module.scss';
+
 export default function Family() {
-  return <div>테스트 페이지 입니다.</div>;
+  return (
+    <section className={styles['family-section']}>
+      <div className={styles.today}>{CalendarInstance.getToday()}</div>
+    </section>
+  );
 }
+
+/**
+ * !
+ */
+
+/**
+ * ! 다이어리 is
+ * ! 성장기록 is
+ * ! 일정기록 is
+ */
