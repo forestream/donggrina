b615101a18ece0728bd18b89d21c352ead4f9cb2
d5ng <dong5ffice@gmail.com>
2024-06-12 01:24:01 +0900

    [#79] Design: 패밀리 다이어리 영역 UI 추가

--- components/family/family-diary.tsx
+++ components/family/family-diary.tsx
@@ -1,6 +1,31 @@
 import React from 'react';
+import Image from 'next/image';
 import styles from './family-diary.module.scss';
 
 export default function FamilyDiary() {
-  return <div></div>;
+  const isImage = false;
+  const isImageClassName = isImage ? `${styles['diary-card']} ${styles.isImage}` : styles['diary-card'];
+
+  return (
+    <div className={styles['wrapper']}>
+      <div className={isImageClassName}>
+        <div className={styles['diary-card__profile']}>
+          <div className={styles['diary-card__avatar']}>
+            <Image src="/images/family/default-image.svg" alt="" fill />
+          </div>
+          <span className={styles['diary-card__username']}>배수지</span>
+        </div>
+        <div className={styles['diary-card__contents']}>
+          <p className={styles['content-text']}>
+            오늘 뽀삐랑 같이 산책을 다녀왔어!오늘 뽀삐랑 같이 산책을 다녀왔어! ...오늘 뽀삐랑 같이 산책을 다녀왔어!오늘
+            뽀삐랑 같이 산책을 다녀왔어! ...오늘 뽀삐랑 같이 산책을 다녀왔어!오늘 뽀삐랑 같이 산책을 다녀왔어! ...
+          </p>
+          <div className={styles['content-image']}></div>
+        </div>
+      </div>
+      <div className={styles['diary-image']}>
+        <Image src="/images/family/family-dog-icon.svg" alt="" fill />
+      </div>
+    </div>
+  );
 }
