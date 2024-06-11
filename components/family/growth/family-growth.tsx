20ad3c874ff74e9acfdb365314e74e7e451d00cc
d5ng <dong5ffice@gmail.com>
2024-06-12 02:50:35 +0900

    [#79] Refactor: 파일 분리 리팩토링

--- components/family/growth/family-growth.tsx
+++ components/family/growth/family-growth.tsx
@@ -1,8 +1,7 @@
-import Link from 'next/link';
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { AvatarImage } from '../../avatar/avatar';
 import 'swiper/css';
 import styles from './family-growth.module.scss';
+import FamilyGrowthItem from './family-growth-item';
 
 export default function FamilyGrowth() {
   return (
@@ -26,15 +25,5 @@ export default function FamilyGrowth() {
         </SwiperSlide>
       </Swiper>
     </section>
-  );
-}
-
-function FamilyGrowthItem() {
-  return (
-    <Link href="/Hello" className={styles['growth-item__layout']}>
-      <div className={styles['growth-item__type']}>{/* 이미지 들어갈 곳. */}</div>
-      <AvatarImage border="main" />
-      <AvatarImage border="main" />
-    </Link>
   );
 }
--- components/family/schedule/family-schedule.tsx
+++ components/family/schedule/family-schedule.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import Avatar from '@/components/avatar/avatar';
 import styles from './family-schedule.module.scss';
+import FamilyScheduleItem from './family-schedule-item';
 
 export default function FamilySchedule() {
   return (
@@ -12,20 +12,5 @@ export default function FamilySchedule() {
         <FamilyScheduleItem />
       </ul>
     </section>
-  );
-}
-
-function FamilyScheduleItem() {
-  return (
-    <li className={styles['schedule-list__item']}>
-      <div className={styles['schedule-item__type']}></div>
-      <div className={styles['schedule-item__content']}>
-        <div className={styles['content-todo']}>산책 (09:00)</div>
-        <Avatar border="gray" />
-      </div>
-      <div className={styles['schedule-item__input']}>
-        <input type="checkbox" />
-      </div>
-    </li>
   );
 }
