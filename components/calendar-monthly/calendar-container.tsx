bc232eb3064c46d1187f5cfdb52583276bb0aa40
hanbin <rj2303@naver.com>
2024-06-12 16:18:11 +0900

    [#44] Refactor: 달력 일정 개수 표시 아이콘 렌더링 방식 변경
    map 메서드 중첩 없애고 Array 객체만 생성

--- components/calendar-monthly/calendar-container.tsx
+++ components/calendar-monthly/calendar-container.tsx
@@ -3,13 +3,36 @@ import getSeventhDate from '@/utils/get-seventh-date';
 import { useCalendarContext } from '../calendar-compound/calendar';
 import getCalendarArray from '@/utils/get-calendar-array';
 
+const MOCK = {
+  code: 200,
+  message: '일정 목록 월별 조회 성공',
+  data: [
+    {
+      date: '2024-06-07',
+      count: 1,
+    },
+    {
+      date: '2024-06-10',
+      count: 2,
+    },
+    {
+      date: '2024-06-11',
+      count: 4,
+    },
+    {
+      date: '2024-06-21',
+      count: 2,
+    },
+  ],
+};
+
 export default function CalendarContainer() {
   const calendarContext = useCalendarContext();
   const year = calendarContext.year;
   const month = calendarContext.month + 1;
   const date = calendarContext.date;
 
-  const calendarArray = getCalendarArray(year, month);
+  const { calendarArray, todoCountsArray } = getCalendarArray(year, month, MOCK.data);
 
   return (
     <div className={styles.container}>
@@ -26,9 +49,7 @@ export default function CalendarContainer() {
           >
             <div className={`${styles.date} ${calendarCell == date ? styles.selected : ''}`}>{calendarCell}</div>
             <div className={styles.todoIconContainer}>
-              <div className={styles.todoIcon}></div>
-              <div className={styles.todoIcon}></div>
-              <div className={styles.todoIcon}></div>
+              {Array(Math.min(3, todoCountsArray[i])).fill(<div className={styles.todoIcon}></div>)}
             </div>
           </div>
         ),
--- utils/get-calendar-array.ts
+++ utils/get-calendar-array.ts
@@ -2,14 +2,29 @@ import { CALENDAR_DAYS, CALENDAR_EMPTY_DATES } from '@/lib/constants/calendar-co
 import getDateCount from './get-date-count';
 import getFirstDay from './get-first-day';
 
-export default function getCalendarArray(year: number, month: number) {
-  const dateCount = getDateCount(year, month);
+export default function getCalendarArray(
+  year: number,
+  month: number,
+  todoCounts: { date: string; count: number }[] = [],
+) {
   const firstDay = getFirstDay(year, month);
   const emptyDates = Array(CALENDAR_EMPTY_DATES[firstDay]).fill('');
+
+  const dateCount = getDateCount(year, month);
   const dates = Array(dateCount)
     .fill(0)
     .map((_, i) => i + 1);
-  const calendarArray = [...CALENDAR_DAYS, ...emptyDates, ...dates];
 
-  return calendarArray;
+  const editedTodoCounts = todoCounts.map(({ date, count }) => ({
+    date: +date.split('-')[2],
+    count,
+  }));
+  const counts = Array(dateCount).fill(0);
+  editedTodoCounts.forEach(({ date, count }, i) => {
+    counts[date - 1] = count;
+  });
+
+  const calendarArray = [...CALENDAR_DAYS, ...emptyDates, ...dates];
+  const todoCountsArray = [...CALENDAR_DAYS, ...emptyDates, ...counts];
+  return { calendarArray, todoCountsArray };
 }
