95ea1dd657786bf1b749993a93513072a1232d0e
hanbin <rj2303@naver.com>
2024-06-12 13:52:39 +0900

    [#44] Feat: 우하단 추가 버튼 공용화
    다이어리, 성장기록, 일정기록에 쓰일 버튼 공용화

--- components/calendar-monthly/create-todo-button.tsx
+++ components/calendar-monthly/create-todo-button.tsx
@@ -1,7 +1,5 @@
-import Image from 'next/image';
-import styles from './create-todo-button.module.scss';
-import Link from 'next/link';
 import { useCalendarContext } from '../calendar-compound/calendar';
+import AddButton from '../common/add-button/add-button';
 
 export default function CreateTodoButton() {
   const calendarContext = useCalendarContext();
@@ -9,9 +7,5 @@ export default function CreateTodoButton() {
   const month = calendarContext.month + 1;
   const date = calendarContext.date;
 
-  return (
-    <Link href={`/calendar/create?year=${year}&month=${month}&date=${date}`} className={styles.outer}>
-      <Image src="images/calendar/new-todo.svg" alt="할일 추가 버튼" width={20} height={20} />
-    </Link>
-  );
+  return <AddButton href={`/calendar/create?year=${year}&month=${month}&date=${date}`} />;
 }
