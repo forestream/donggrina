b2adcb3ea6f064d09bfe86a5b0ad1231595960da
hanbin <rj2303@naver.com>
2024-06-12 18:30:34 +0900

    [#44] Feat: 일정 등록 페이지 날짜 시간 input 수정
    기존 쿼리값에서 가져오던 연월일 삭제
    연월일시분 초기값 null로 설정
    연월일시분 각각의 인풋으로 나눠서 관리하던 부분(6개)을 하나의 인풋으로 통합.
    get-date-time-frontend 함수에서 사용자가 입력한 시각을 문자열로 반환. 필요한 값들 중 하나라도 없으면 undefined 얼리 리턴

--- pages/calendar/create/index.tsx
+++ pages/calendar/create/index.tsx
@@ -1,8 +1,4 @@
 import styles from './create.module.scss';
-import validateDate from '@/utils/validate-date';
-import validateMonth from '@/utils/validate-month';
-import validateYear from '@/utils/validate-year';
-import { GetServerSidePropsContext } from 'next';
 import { useState } from 'react';
 import { CalendarProps } from '..';
 import useModal from '@/hooks/use-modal';
@@ -10,31 +6,13 @@ import CalendarModal from '@/components/calendar-monthly/calendar-modal';
 import { SubmitHandler, useForm } from 'react-hook-form';
 import PetRadio from '@/components/calendar-monthly/pet-radio';
 import { TODO_CATEGORY } from '@/lib/constants/calendar-constants';
-
-export async function getServerSideProps(context: GetServerSidePropsContext) {
-  let {
-    query: { year, month, date },
-  } = context;
-
-  if (typeof year === 'object') year = year[0];
-  if (typeof month === 'object') month = month[0];
-  if (typeof date === 'object') date = date[0];
-
-  const today = new Date();
-  const [yearToday, monthToday] = [today.getFullYear(), today.getMonth() + 1];
-
-  if (!validateYear(year)) year = String(yearToday);
-  if (!validateMonth(month)) month = String(monthToday);
-  if (!validateDate(date)) date = '1';
-
-  return { props: { year: +(year as string), month: +(month as string), date: +(date as string) } };
-}
+import getDateTimeFrontend from '@/utils/get-date-time-frontend';
 
 export interface DateTime extends CalendarProps {
-  [key: string]: string | number;
-  ampm: string;
-  hour: number;
-  minute: number;
+  [key: string]: string | number | null;
+  ampm: string | null;
+  hour: number | null;
+  minute: number | null;
 }
 
 export interface IFormInput extends CalendarProps {
@@ -42,16 +20,17 @@ export interface IFormInput extends CalendarProps {
   memo: string;
   pet: string;
   category: string;
+  dateTime: string;
 }
 
-export default function Create({ year, month, date }: CalendarProps) {
+export default function Create() {
   const [dateTime, setDateTime] = useState<DateTime>({
-    year,
-    month,
-    date,
-    ampm: '오전',
-    hour: 12,
-    minute: 0,
+    year: null,
+    month: null,
+    date: null,
+    ampm: null,
+    hour: null,
+    minute: null,
   });
 
   const updateDateTime = (newDateTime: DateTime) => {
@@ -72,7 +51,6 @@ export default function Create({ year, month, date }: CalendarProps) {
     handleSubmit,
     formState: { errors, isValid },
   } = useForm<IFormInput>();
-  console.log(isValid);
 
   return (
     <div className={styles.outer}>
@@ -127,17 +105,15 @@ export default function Create({ year, month, date }: CalendarProps) {
             날짜 / 시간
           </button>
           <div className={styles.todoDateSelector}>
-            {Object.keys(dateTime).map((key) => (
-              <div key={key} className={`${styles.input} ${styles[key]}`}>
-                {dateTime[key].toString().padStart(2, '0')}
-                <input
-                  {...register(key as keyof IFormInput, { required: true })}
-                  type="hidden"
-                  id={key}
-                  value={dateTime[key].toString().padStart(2, '0')}
-                />
-              </div>
-            ))}
+            <input
+              {...register('dateTime', { required: true })}
+              type="text"
+              id="dateTime"
+              placeholder="YYYY-MM-DD 오전/오후 00:00"
+              value={getDateTimeFrontend(dateTime)}
+              className={styles.input}
+              disabled
+            />
           </div>
         </div>
         <button className={styles.submit} disabled={!isValid}>
---
+++ utils/get-date-time-frontend.ts
@@ -0,0 +1,9 @@
+import { DateTime } from '@/pages/calendar/create';
+
+export default function getDateTimeFrontend(dateTime: DateTime) {
+  if (Object.values(dateTime).includes(null)) return;
+
+  const { year, month, date, ampm, hour, minute } = dateTime;
+
+  return `${year}-${month}-${date} ${ampm} ${hour}:${minute}`;
+}
