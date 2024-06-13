2622636301266a961be7aa59e214181f630a6136
hanbin <rj2303@naver.com>
2024-06-13 11:00:51 +0900

    [#44] Feat: 일자별 일정 개수 받는 api 구현

--- api/calendar/request.ts
+++ api/calendar/request.ts
@@ -1,6 +1,12 @@
 import { IFormInput } from '@/pages/calendar/create';
 import { axiosInstance } from '..';
 
+export async function fetchTodos(yearMonth: string) {
+  console.log(yearMonth);
+  const { data } = await axiosInstance.get(`/calendar/month?yearMonth=${yearMonth}`);
+  return data.data;
+}
+
 export async function postTodo(data: IFormInput) {
   console.log(data);
   const response = await axiosInstance.post('/calendar', data);
--- components/calendar-monthly/calendar-container.tsx
+++ components/calendar-monthly/calendar-container.tsx
@@ -3,42 +3,32 @@ import getSeventhDate from '@/utils/get-seventh-date';
 import { useCalendarContext } from '../calendar-compound/calendar';
 import getCalendarArray from '@/utils/get-calendar-array';
 import classNames from 'classnames';
-
-const MOCK = {
-  code: 200,
-  message: '일정 목록 월별 조회 성공',
-  data: [
-    {
-      date: '2024-06-07',
-      count: 1,
-    },
-    {
-      date: '2024-06-10',
-      count: 2,
-    },
-    {
-      date: '2024-06-11',
-      count: 4,
-    },
-    {
-      date: '2024-06-21',
-      count: 2,
-    },
-  ],
-};
+import { useEffect, useState } from 'react';
+import { fetchTodos } from '@/api/calendar/request';
 
 export default function CalendarContainer() {
+  const [monthlyTodos, setMonthlyTodos] = useState();
   const calendarContext = useCalendarContext();
   const year = calendarContext.year;
   const month = calendarContext.month + 1;
   const date = calendarContext.date;
+  const yearMonth = `${year}-${month.toString().padStart(2, '0')}`;
 
-  const { calendarArray, todoCountsArray } = getCalendarArray(year, month, MOCK.data);
+  const { calendarArray, todoCountsArray } = getCalendarArray(year, month, monthlyTodos);
 
   const calendarCellClassNames = (cellIndex: number) =>
     classNames(styles.calendarCell, {
       [styles.red]: getSeventhDate(cellIndex),
     });
+
+  const handleLoad = async () => {
+    const data = await fetchTodos(yearMonth);
+    setMonthlyTodos(data);
+  };
+
+  useEffect(() => {
+    handleLoad();
+  }, [year, month]);
 
   return (
     <div className={styles.container}>
