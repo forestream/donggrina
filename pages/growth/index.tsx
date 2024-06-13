84bcdce447ec08b33e6d4f14493c433a428c9db1
mun-jihye <m84422583@gmail.com>
2024-06-13 17:01:10 +0900

    feat: convert-to-localdate util로 분리

--- pages/growth/index.tsx
+++ pages/growth/index.tsx
@@ -4,11 +4,15 @@ import Calendar, { useCalendarContext } from '@/components/calendar-compound/cal
 import styles from './growth.module.scss';
 import getDay from '@/utils/get-day';
 import AddButton from '@/components/common/add-button/add-button';
+import { useGetGrotwthByDateQuery } from '@/hooks/queries/growth/useGetGrowthQueries';
+import { convertToLocalDate } from '@/utils/convert-local-date';
 
 export default function GrowthPage() {
   const { year, month, date } = useCalendarContext();
   console.log(year, month, date);
   const day = getDay(year, month, date);
+  const localDate = convertToLocalDate({ year, month, date });
+  const { data: growthLists } = useGetGrotwthByDateQuery(localDate);
   return (
     <>
       <Calendar>
