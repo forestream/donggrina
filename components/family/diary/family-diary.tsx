044499355cbf93b8246d41045d9a8ed2cad15065
d5ng <dong5ffice@gmail.com>
2024-06-14 14:50:33 +0900

    [#79] Feat: 다이어리 없을 때 UI 추가 및 리팩토링

--- components/family/diary/family-diary.tsx
+++ components/family/diary/family-diary.tsx
@@ -1,32 +1,13 @@
 import React from 'react';
-import Image from 'next/image';
-import Avatar from '@/components/avatar/avatar';
-import FamilyDiaryEmpty from '@/components/family/diary/family-diary-empty';
-import styles from './family-diary.module.scss';
+import FamilyDiaryLayout from './layout/family-diary-layout';
+import FamilyDiaryContents from './family-diary-contents';
+import FamilyDiaryContentsEmpty from '@/components/family/diary/family-diary-contents-empty';
 
 export default function FamilyDiary() {
-  const isImage = true;
-  const isImageClassName = isImage ? `${styles['diary-card']} ${styles.isImage}` : styles['diary-card'];
-
-  return <FamilyDiaryEmpty />;
-
   return (
-    <div className={styles['wrapper']}>
-      <div className={isImageClassName}>
-        <div className={styles['diary-card__profile']}>
-          <Avatar />
-        </div>
-        <div className={styles['diary-card__contents']}>
-          <p className={styles['content-text']}>
-            오늘 뽀삐랑 같이 산책을 다녀왔어!오늘 뽀삐랑 같이 산책을 다녀왔어! ...오늘 뽀삐랑 같이 산책을 다녀왔어!오늘
-            뽀삐랑 같이 산책을 다녀왔어! ...오늘 뽀삐랑 같이 산책을 다녀왔어!오늘 뽀삐랑 같이 산책을 다녀왔어! ...
-          </p>
-          <div className={styles['content-image']}></div>
-        </div>
-      </div>
-      <div className={styles['diary-image']}>
-        <Image src="/images/family/family-dog-icon.svg" alt="" fill />
-      </div>
-    </div>
+    <FamilyDiaryLayout>
+      {/* <FamilyDiaryContentsEmpty /> */}
+      <FamilyDiaryContents />
+    </FamilyDiaryLayout>
   );
 }
