37e1d74a1593a5065915ef14ec39502a1793635b
mun-jihye <m84422583@gmail.com>
2024-06-14 23:11:22 +0900

    [#70] delete: list-footer 삭제

--- components/growth/list/index.tsx
+++ components/growth/list/index.tsx
@@ -1,26 +1,22 @@
-// import React from 'react';
-// import styles from './list.module.scss';
-// import Link from 'next/link';
-// import ListHeader from './list-header';
-// import ListFooter from './list-footer';
+import React from 'react';
+import styles from './list.module.scss';
+import Link from 'next/link';
+import ListHeader from './list-header';
 
-// interface GrowthListProps {
-//   text: string;
-//   writer: string;
-//   pet: string;
-//   categoryName: string;
-//   heartCount: number;
-//   chatCount: number;
-// }
+interface GrowthListProps {
+  text: string;
+  writer: string;
+  pet: string;
+  categoryName: string;
+}
 
-// export default function GrowthList({ text, writer, pet, categoryName, heartCount, chatCount }: GrowthListProps) {
-//   return (
-//     <div className={styles.container}>
-//       <ListHeader writer={writer} categoryName={categoryName} pet={pet} />
-//       <Link href="/" className={styles.text}>
-//         text text text text text text text text text text texttext texttext texttext text text text text
-//       </Link>
-//       <ListFooter heartCount={heartCount} chatCount={chatCount} />
-//     </div>
-//   );
-// }
+export default function GrowthList({ text, writer, pet, categoryName }: GrowthListProps) {
+  return (
+    <div className={styles.container}>
+      <ListHeader writer={writer} categoryName={categoryName} pet={pet} />
+      <Link href="/" className={styles.text}>
+        text text text text text text text text text text texttext texttext texttext text text text text
+      </Link>
+    </div>
+  );
+}
--- pages/growth/index.tsx
+++ pages/growth/index.tsx
@@ -1,39 +1,39 @@
-// import React from 'react';
-// import GrowthList from '@/components/growth/list';
-// import Calendar, { useCalendarContext } from '@/components/calendar-compound/calendar';
-// import styles from './growth.module.scss';
-// import getDay from '@/utils/get-day';
-// import AddButton from '@/components/common/add-button/add-button';
-// import { useGetGrotwthByDateQuery } from '@/hooks/queries/growth/useGetGrowthQueries';
-// import { convertToLocalDate } from '@/utils/convert-local-date';
+import React from 'react';
+import GrowthList from '@/components/growth/list';
+import Calendar, { useCalendarContext } from '@/components/calendar-compound/calendar';
+import styles from './growth.module.scss';
+import getDay from '@/utils/get-day';
+import AddButton from '@/components/common/add-button/add-button';
+import { useGetGrotwthByDateQuery } from '@/hooks/queries/growth/useGetGrowthQueries';
+import { convertToLocalDate } from '@/utils/convert-local-date';
 
 export default function GrowthPage() {
-  //   const { year, month, date } = useCalendarContext();
-  //   console.log(year, month, date);
-  //   const day = getDay(year, month, date);
-  //   const localDate = convertToLocalDate({ year, month, date });
-  //   const { data: growthLists } = useGetGrotwthByDateQuery(localDate);
-  //   return (
-  //     <>
-  //       <Calendar>
-  //         <Calendar.Header>성장 기록</Calendar.Header>
-  //         <Calendar.Year />
-  //         <Calendar.Month />
-  //         <Calendar.Weekly />
-  //       </Calendar>
-  //       <div className={styles.wrapper}>
-  //         <p className={styles.date}>
-  //           {month}월 {date}일 {day}
-  //         </p>
-  //         <div className={styles.listContainer}>
-  //           <GrowthList />
-  //           <GrowthList />
-  //           <GrowthList />
-  //           <GrowthList />
-  //           <GrowthList />
-  //         </div>
-  //         <AddButton href={'/'} />
-  //       </div>
-  //     </>
-  //   );
+  const { year, month, date } = useCalendarContext();
+  console.log(year, month, date);
+  const day = getDay(year, month, date);
+  const localDate = convertToLocalDate({ year, month, date });
+  const { data: growthLists } = useGetGrotwthByDateQuery(localDate);
+  return (
+    <>
+      <Calendar>
+        <Calendar.Header>성장 기록</Calendar.Header>
+        <Calendar.Year />
+        <Calendar.Month />
+        <Calendar.Weekly />
+      </Calendar>
+      <div className={styles.wrapper}>
+        <p className={styles.date}>
+          {month}월 {date}일 {day}
+        </p>
+        <div className={styles.listContainer}>
+          <GrowthList />
+          <GrowthList />
+          <GrowthList />
+          <GrowthList />
+          <GrowthList />
+        </div>
+        <AddButton href={'/'} />
+      </div>
+    </>
+  );
 }
