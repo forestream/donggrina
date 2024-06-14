a8f0f2d891e5302b7524bef4a1437a7d06427e7b
hanbin <rj2303@naver.com>
2024-06-14 15:45:10 +0900

    [#44] Design: 이미지 border radius 설정. js doc 작성

--- components/calendar-monthly/pet-radio.tsx
+++ components/calendar-monthly/pet-radio.tsx
@@ -9,17 +9,22 @@ interface PetRadio {
   petImage: string;
 }
 
+/**
+ * @param register - react-hook-form의 register 함수를 넣어주세요.
+ * @param {string} petName - 반려동물 이름
+ * @param {string} petImage - 반려동물 이미지 url
+ */
 export default function PetRadio({ register, petName, petImage }: PetRadio) {
   return (
     <label className={styles.petLabel}>
-      <div className={styles.petImage}>
+      <div className={styles.petImageContainer}>
         <input
           {...register('petName', { validate: (selected) => !!selected || '*반려동물을 선택해주세요.' })}
           value={petName}
           className={styles.petInput}
           type="radio"
         />
-        <Image src={petImage} alt="반려동물 프로필 사진" fill />
+        <Image className={styles.petImage} src={petImage} alt="반려동물 프로필 사진" fill />
         <Image
           src="/images/calendar/check-circle.svg"
           alt="체크 표시"
