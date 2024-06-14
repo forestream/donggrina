1e77238bb6754d6e2d22d5268ce8325ba5a756c4
d5ng <dong5ffice@gmail.com>
2024-06-14 16:57:17 +0900

    [#79] Feat: 성장기록 없을 때 UI 추가

--- components/family/growth/family-growth.tsx
+++ components/family/growth/family-growth.tsx
@@ -2,12 +2,24 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import styles from './family-growth.module.scss';
 import FamilyGrowthItem from './family-growth-item';
+import FamilyGrowthEmpty from './family-growth-empty';
+
+// {
+//   "id": 2,
+//   "writerProfileImageUrl": "qruwoerwfq.jpeg",
+//   "petProfileImageUrl": "qwlrqqwerpowu.jpeg",
+//   "category": "간식",
+//   "dateTime": "2024-06-04T16:39:19.221816",
+//   "nickname": "왕꿈틀이",
+//   "isMine": false
+//  },
 
 export default function FamilyGrowth() {
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.title}>성장기록</h2>
-      <Swiper className={styles['growth-list']} slidesPerView="auto" spaceBetween={10} wrapperTag="ul">
+      <FamilyGrowthEmpty />
+      {/* <Swiper className={styles['growth-list']} slidesPerView="auto" spaceBetween={10} wrapperTag="ul">
         <SwiperSlide className={styles['growth-list__item']} tag="li">
           <FamilyGrowthItem />
         </SwiperSlide>
@@ -23,7 +35,7 @@ export default function FamilyGrowth() {
         <SwiperSlide className={styles['growth-list__item']} tag="li">
           <FamilyGrowthItem />
         </SwiperSlide>
-      </Swiper>
+      </Swiper> */}
     </section>
   );
 }
