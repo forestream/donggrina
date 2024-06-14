17c2be05f2f61e8b530e8d8d1f4411746eb252a0
mun-jihye <m84422583@gmail.com>
2024-06-14 13:38:12 +0900

    build 오류 주석 처리로 해결

--- pages/growth/index.tsx
+++ pages/growth/index.tsx
@@ -7,33 +7,33 @@
 // import { useGetGrotwthByDateQuery } from '@/hooks/queries/growth/useGetGrowthQueries';
 // import { convertToLocalDate } from '@/utils/convert-local-date';
 
-// export default function GrowthPage() {
-//   const { year, month, date } = useCalendarContext();
-//   console.log(year, month, date);
-//   const day = getDay(year, month, date);
-//   const localDate = convertToLocalDate({ year, month, date });
-//   const { data: growthLists } = useGetGrotwthByDateQuery(localDate);
-//   return (
-//     <>
-//       <Calendar>
-//         <Calendar.Header>성장 기록</Calendar.Header>
-//         <Calendar.Year />
-//         <Calendar.Month />
-//         <Calendar.Weekly />
-//       </Calendar>
-//       <div className={styles.wrapper}>
-//         <p className={styles.date}>
-//           {month}월 {date}일 {day}
-//         </p>
-//         <div className={styles.listContainer}>
-//           <GrowthList />
-//           <GrowthList />
-//           <GrowthList />
-//           <GrowthList />
-//           <GrowthList />
-//         </div>
-//         <AddButton href={'/'} />
-//       </div>
-//     </>
-//   );
-// }
+export default function GrowthPage() {
+  //   const { year, month, date } = useCalendarContext();
+  //   console.log(year, month, date);
+  //   const day = getDay(year, month, date);
+  //   const localDate = convertToLocalDate({ year, month, date });
+  //   const { data: growthLists } = useGetGrotwthByDateQuery(localDate);
+  //   return (
+  //     <>
+  //       <Calendar>
+  //         <Calendar.Header>성장 기록</Calendar.Header>
+  //         <Calendar.Year />
+  //         <Calendar.Month />
+  //         <Calendar.Weekly />
+  //       </Calendar>
+  //       <div className={styles.wrapper}>
+  //         <p className={styles.date}>
+  //           {month}월 {date}일 {day}
+  //         </p>
+  //         <div className={styles.listContainer}>
+  //           <GrowthList />
+  //           <GrowthList />
+  //           <GrowthList />
+  //           <GrowthList />
+  //           <GrowthList />
+  //         </div>
+  //         <AddButton href={'/'} />
+  //       </div>
+  //     </>
+  //   );
+}
