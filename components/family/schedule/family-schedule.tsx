300b4a572f094751ab9a799df34ad51bdb6b2a7c
d5ng <dong5ffice@gmail.com>
2024-06-16 01:22:32 +0900

    [#105] Feat: 일정기록 데이터 받아오기

--- components/family/schedule/family-schedule.tsx
+++ components/family/schedule/family-schedule.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import FamilyScheduleItem from './family-schedule-item';
+<<<<<<< Updated upstream
 import FamilyScheduleEmpty from './family-schedule-empty';
+=======
+>>>>>>> Stashed changes
 import { useFetchSchedule } from '@/hooks/queries/family';
 import styles from './family-schedule.module.scss';
 
 export default function FamilySchedule() {
   const scheduleQueries = useFetchSchedule('2024-06-16');
 
+<<<<<<< Updated upstream
   if (!scheduleQueries.data) return;
+=======
+  console.log(scheduleQueries.data);
+>>>>>>> Stashed changes
 
   return (
     <section className={styles.wrapper}>
