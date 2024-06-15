68cb246a9666d7aea34bf69851afe2ffa3bd6bac
김경주 <popo505@naver.com>
2024-06-15 18:27:38 +0900

    [#104] feat: 다이어리 등록 페이지 사진 등록 구현

--- components/diaries/diary-create/diary-create.tsx
+++ components/diaries/diary-create/diary-create.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { useForm } from 'react-hook-form';
 import classNames from 'classnames';
 import { WEATHER_TYPES } from '@/lib/constants/diaries-constants';
@@ -11,44 +11,92 @@ const DiaryCreate = () => {
     watch,
     setValue,
     formState: { errors, isValid },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      content: '',
+      weather: '',
+      photos: Array(5).fill(null),
+    },
+  });
   const selectedWeather = watch('weather');
+  const imagePreviews = watch('photos');
+  const fileInputRefs = useRef(new Array(5).fill(null));
+
+  const handleImageChange = (index: number) => (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files && files.length > 0) {
+      const file = files[0];
+      const updatedPhotos = [...imagePreviews];
+      updatedPhotos[index] = URL.createObjectURL(file);
+      setValue('photos', updatedPhotos);
+    }
+  };
+
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
-    <div>
-      <form onSubmit={handleSubmit((data) => console.log(data))}>
+    <div className={styles.container}>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <textarea
           {...register('content', { required: '*내용을 입력해주세요.' })}
           className={styles.content}
           id="content"
-          placeholder={`메모\n어떤 일정인지 자세하게 기록하실 수 있어요!`}
+          placeholder={'메모\n어떤 일정인지 자세하게 기록하실 수 있어요!'}
         />
-        {errors.content && <p className={styles.error}>{errors.content.message as string}</p>}
+        {errors.content && <p className={styles.error}>{errors.content.message}</p>}
 
-        <div className={styles.weather}>
-          <img src="/images/diaries/bar.svg" />
-          날씨
+        <hr className={styles.division} />
+
+        <div className={styles.weatherContainer}>
+          <div className={styles.weather}>
+            <img src="/images/diaries/bar.svg" alt="Weather bar" />
+            날씨
+          </div>
+          <div className={styles.weatherIcons}>
+            {WEATHER_TYPES.map((weather) => (
+              <button
+                key={weather.id}
+                type="button"
+                onClick={() => setValue('weather', weather.id)}
+                className={selectedWeather === weather.id ? styles.selected : ''}
+              >
+                <img src={selectedWeather === weather.id ? weather.selectedIcon : weather.icon} alt={weather.label} />
+              </button>
+            ))}
+          </div>
         </div>
-        <div className={styles.weatherIcons}>
-          {WEATHER_TYPES.map((weather) => (
-            <button
-              key={weather.id}
-              type="button"
-              onClick={() => setValue('weather', weather.id)}
-              className={selectedWeather === weather.id ? styles.selected : ''}
-            >
-              <img src={selectedWeather === weather.id ? weather.selectedIcon : weather.icon} alt={weather.label} />
-            </button>
+
+        <div className={styles.imagesContainer}>
+          {imagePreviews.map((src, index) => (
+            <div key={index}>
+              <input
+                ref={(el) => (fileInputRefs.current[index] = el)}
+                type="file"
+                accept="image/*"
+                onChange={handleImageChange(index)}
+                hidden
+              />
+              <img
+                src={src || '/images/diaries/imageupload-default.svg'}
+                alt="default image"
+                className={styles.images}
+                onClick={() => fileInputRefs.current[index]?.click()}
+              />
+            </div>
           ))}
         </div>
 
-        <button
-          className={classNames(styles.submit, {
-            [styles.disabled]: !isValid,
-          })}
-        >
-          등록하기
-        </button>
+        <div className={styles.button}>
+          <button
+            className={classNames(styles.submit, {
+              [styles.disabled]: !isValid,
+            })}
+          >
+            등록하기
+          </button>
+        </div>
       </form>
     </div>
   );
