9b0b216254a116abc0315d7ea96860aa4fbae1c7
김경주 <popo505@naver.com>
2024-06-15 16:12:59 +0900

    [#104] feat: 다이어리 등록 페이지 날씨 구현

--- components/diaries/diary-create/diary-create.tsx
+++ components/diaries/diary-create/diary-create.tsx
@@ -1,14 +1,18 @@
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import classNames from 'classnames';
+import { WEATHER_TYPES } from '@/lib/constants/diaries-constants';
 import styles from './diary-create.module.scss';
 
 const DiaryCreate = () => {
   const {
     register,
     handleSubmit,
+    watch,
+    setValue,
     formState: { errors, isValid },
   } = useForm();
+  const selectedWeather = watch('weather');
 
   return (
     <div>
@@ -20,6 +24,23 @@ const DiaryCreate = () => {
           placeholder={`메모\n어떤 일정인지 자세하게 기록하실 수 있어요!`}
         />
         {errors.content && <p className={styles.error}>{errors.content.message as string}</p>}
+
+        <div className={styles.weather}>
+          <img src="/images/diaries/bar.svg" />
+          날씨
+        </div>
+        <div className={styles.weatherIcons}>
+          {WEATHER_TYPES.map((weather) => (
+            <button
+              key={weather.id}
+              type="button"
+              onClick={() => setValue('weather', weather.id)}
+              className={selectedWeather === weather.id ? styles.selected : ''}
+            >
+              <img src={selectedWeather === weather.id ? weather.selectedIcon : weather.icon} alt={weather.label} />
+            </button>
+          ))}
+        </div>
 
         <button
           className={classNames(styles.submit, {
