eecd71f09758c5b6591f6e839d0f81dfaa20c34a
mun-jihye <m84422583@gmail.com>
2024-06-16 23:51:19 +0900

    [#82] design: 카테고리 별 인풋 디자인

--- pages/growth/create/category-inputs/index.tsx
+++ pages/growth/create/category-inputs/index.tsx
@@ -12,26 +12,26 @@ export default function CategoryInputs({ selectedCategory, register }: CategoryI
     case '간식':
       return (
         <div className={styles.inputGroup}>
-          <label>간식</label>
+          <label className={styles.label}>간식*</label>
           <input {...register('content.snack')} placeholder="간식의 이름을 입력해주세요" className={styles.input} />
         </div>
       );
     case '이상 증상':
       return (
         <div className={styles.inputGroup}>
-          <label>이상 증상</label>
+          <label className={styles.label}>이상 증상*</label>
           <input
             {...register('content.abnormalSymptom')}
-            placeholder="이상증상을 입력해주세요"
+            placeholder="이상 증상을 입력해주세요"
             className={styles.input}
           />
         </div>
       );
     case '병원 기록':
       return (
-        <>
+        <div className={styles.wrapper}>
           <div className={styles.inputGroup}>
-            <label>병원명</label>
+            <label className={styles.label}>병원명</label>
             <input
               {...register('content.hospitalName')}
               placeholder="병원의 이름을 입력해주세요"
@@ -39,15 +39,15 @@ export default function CategoryInputs({ selectedCategory, register }: CategoryI
             />
           </div>
           <div className={styles.inputGroup}>
-            <label>증상</label>
+            <label className={styles.label}>증상*</label>
             <input {...register('content.symptom')} placeholder="증상을 입력해주세요" className={styles.input} />
           </div>
           <div className={styles.inputGroup}>
-            <label>진료 내용</label>
+            <label className={styles.label}>진료 내용</label>
             <input {...register('content.diagnosis')} placeholder="진료 내용을 입력해주세요" className={styles.input} />
           </div>
           <div className={styles.inputGroup}>
-            <label>복용 방법</label>
+            <label className={styles.label}>복용 방법</label>
             <input
               {...register('content.medicationMethod')}
               placeholder="복용 방법을 입력해주세요"
@@ -55,15 +55,15 @@ export default function CategoryInputs({ selectedCategory, register }: CategoryI
             />
           </div>
           <div className={styles.inputGroup}>
-            <label>금액</label>
+            <label className={styles.label}>금액</label>
             <input {...register('content.price')} placeholder="1,000 ₩" className={styles.input} />
           </div>
-        </>
+        </div>
       );
     default:
       return (
         <div className={styles.inputGroup}>
-          <label>사료</label>
+          <label className={styles.label}>사료*</label>
           <input {...register('content.food')} placeholder="사료의 이름을 입력해주세요" className={styles.input} />
         </div>
       );
