071fb85bace6746f1207bbfa85d66ab41d13e7f0
d5ng <dong5ffice@gmail.com>
2024-06-17 16:27:34 +0900

    [#105] 불필요한 코드 지우기

--- hooks/queries/family/index.ts
+++ hooks/queries/family/index.ts
@@ -1,5 +1,4 @@
 import { axiosInstance } from '@/api';
-import { fetchDailyTodos } from '@/api/calendar/request';
 import { DiaryData } from '@/types/diary';
 import { GrowthData } from '@/types/growth';
 import { useQuery } from '@tanstack/react-query';
@@ -10,13 +9,6 @@ async function fetchDiary(date: string) {
 
 async function getGrowthByDate(date: string) {
   return (await axiosInstance.get<GrowthData[]>(`/growth?date=${date}`)).data;
-}
-
-export function useFetchSchedule(date: string) {
-  return useQuery({
-    queryKey: ['family/calendar'],
-    queryFn: () => fetchDailyTodos(date),
-  });
 }
 
 export function useFetchGrowth(date: string) {
