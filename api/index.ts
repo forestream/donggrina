2268526353cdfc77d3f7c805586cb15d020912a1
LimDong <ilhai6@naver.com>
2024-06-17 19:34:12 +0900

    [#111] Fix: 인터셉터 수정

--- api/index.ts
+++ api/index.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosInstance } from 'axios';
 import { getCookie, setCookie } from 'cookies-next';
 import { reissuanceAt } from './auth';
 
@@ -18,43 +18,50 @@ export const axiosFileInstance = axios.create({
 
 let isRefreshing = false;
 
-axiosInstance.interceptors.request.use(
-  (config) => {
-    const token = getCookie('accessToken');
-    if (token) {
-      config.headers['Authorization'] = `Bearer ${token}`;
-    }
-    return config;
-  },
-  (error) => Promise.reject(error),
-);
+const setupInterceptors = (instance: AxiosInstance) => {
+  instance.interceptors.request.use(
+    (config) => {
+      const token = getCookie('accessToken');
+      if (token) {
+        config.headers['Authorization'] = `Bearer ${token}`;
+      }
+      return config;
+    },
+    (error) => Promise.reject(error),
+  );
 
-axiosInstance.interceptors.response.use(
-  (response) => response,
-  async (error) => {
-    const token = getCookie('accessToken');
-    const originalRequest = error.config;
-    if (error.response.status === 401 && !originalRequest._retry) {
-      originalRequest._retry = true;
-      if (!isRefreshing) {
-        isRefreshing = true;
-        const refresh = getCookie('refreshToken');
-        const data = {
-          accessToken: token!.toString(),
-          refreshToken: refresh!.toString(),
-        };
-        try {
-          const response = await reissuanceAt(data);
-          const newAt = response.data.data;
-          setCookie('accessToken', newAt);
-          axios.defaults.headers['Authorization'] = `Bearer ${newAt}`;
-          originalRequest.headers.Authorization = `Bearer ${newAt}`;
-          return axios(originalRequest);
-        } catch (error) {
-          console.log(`토큰 갱신 실패 ${error}`);
+  instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+      const token = getCookie('accessToken');
+      const originalRequest = error.config;
+      if (error.response.status === 401 && !originalRequest._retry) {
+        originalRequest._retry = true;
+        if (!isRefreshing) {
+          isRefreshing = true;
+          const refresh = getCookie('refreshToken');
+          const data = {
+            accessToken: token!.toString(),
+            refreshToken: refresh!.toString(),
+          };
+          try {
+            const response = await reissuanceAt(data);
+            const newAt = response.data.data;
+            setCookie('accessToken', newAt);
+            axios.defaults.headers['Authorization'] = `Bearer ${newAt}`;
+            originalRequest.headers.Authorization = `Bearer ${newAt}`;
+            isRefreshing = false;
+            return axios(originalRequest);
+          } catch (error) {
+            console.log(`토큰 갱신 실패 ${error}`);
+            isRefreshing = false;
+          }
         }
       }
-    }
-    return Promise.reject(error);
-  },
-);
+      return Promise.reject(error);
+    },
+  );
+};
+
+setupInterceptors(axiosInstance);
+setupInterceptors(axiosFileInstance);
