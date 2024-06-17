7738eca85f90c439682d8042374dda40b6e57e00
d5ng <dong5ffice@gmail.com>
2024-06-17 17:43:36 +0900

    [#118] Feat: 프로필 페이지 UI 작업

---
+++ components/mypage/family/family.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function family() {
+  return <div>family</div>;
+}
---
+++ components/mypage/profile/profile.tsx
@@ -0,0 +1,29 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from '@/components/mypage/profile/profile.module.scss';
+
+export default function Profile() {
+  return (
+    <section className={styles.profile}>
+      <h3>마이 페이지</h3>
+      <div className={styles['profile-layout']}>
+        <div className={styles['image-wrapper']}>
+          <Image src="" alt="" fill className={styles['profile-image']} />
+          <Image
+            src="images/start-pet/fileButton.svg"
+            alt="프로필 이미지 수정하기"
+            className={styles['profile-image-icon']}
+            width={34}
+            height={34}
+          />
+        </div>
+        <div className={styles['profile-name']}>
+          <span>홍길동</span>
+          <Link href="">
+            <Image src="images/edit-gray-icon.svg" alt="닉네임 변경하기" width={17} height={17} />
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
