bee87a6160b451644188be1c7f90516c403eb67f
hanbin <rj2303@naver.com>
2024-06-17 18:27:37 +0900

    [#115] Design: 돋보기 검색 버튼 추가

--- components/search/search-bar.tsx
+++ components/search/search-bar.tsx
@@ -1,5 +1,13 @@
+import Image from 'next/image';
 import styles from './search-bar.module.scss';
 
 export default function SearchBar() {
-  return <input placeholder="검색할 키워드를 입력해주세요." className={styles.input} />;
+  return (
+    <div className={styles.searchBar}>
+      <input placeholder="검색할 키워드를 입력해주세요." className={styles.input} />
+      <button className={styles.submit}>
+        <Image src="/images/search/search.svg" alt="검색하기" width={20} height={20} />
+      </button>
+    </div>
+  );
 }
