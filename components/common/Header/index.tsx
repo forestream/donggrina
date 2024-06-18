d6a9754779b786d58d00899f3624f988cb4e7540
d5ng <dong5ffice@gmail.com>
2024-06-18 18:34:23 +0900

    [#131] Feat: 헤더 메인페이지로 이동 가능하게 변경

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image';
 import styles from './Header.module.scss';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 export function Header() {
   const router = useRouter();
@@ -14,7 +15,9 @@ export function Header() {
         <Image src="/images/header/arrow-left-black.svg" alt="뒤로 가기" fill />
       </button>
       <div className={styles.imageBox}>
-        <Image src="/images/header/logo.svg" alt="로고" fill />
+        <Link href="/family">
+          <Image src="/images/header/logo.svg" alt="로고" fill />
+        </Link>
       </div>
     </header>
   );
