70e7d90c61b7e0ad32b6d74f142fc3bbd9f54ba1
mun-jihye <m84422583@gmail.com>
2024-06-19 21:49:51 +0900

    [#141] feat: 성장기록 상세 레이아웃 잡기

--- pages/growth/[growthId]/index.tsx
+++ pages/growth/[growthId]/index.tsx
@@ -5,6 +5,13 @@ import useToggle from '@/hooks/use-toggle';
 import { GetServerSidePropsContext } from 'next';
 import { Params } from 'next/dist/shared/lib/router/utils/route-matcher';
 import { useGetGrowthDetailQuery } from '@/hooks/queries/growth/use-get-growth-queries';
+import CalendarTodoProfile from '@/components/calendar-monthly/calendar-todo-profile';
+import { useDeleteGrowthMutation } from '@/hooks/queries/growth/use-post-growth-query';
+import { useRouter } from 'next/router';
+import useModal from '@/hooks/use-modal';
+import Image from 'next/image';
+import CompleteModal from '../create/complete-modal';
+import { GrowthDetailsData } from '@/types/growth/details';
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
   const {
@@ -20,8 +27,29 @@ interface GrowthDetailPageProps {
 
 export default function GrowthDetailPage({ growthId }: GrowthDetailPageProps) {
   const { isToggle: isOpen, handleCloseToggle: onCloseToggle, handleOpenToggle: onOpenToggle } = useToggle();
-  const { data: growthData } = useGetGrowthDetailQuery(growthId);
-  console.log(growthData?.data);
+  const { data: growthDatas } = useGetGrowthDetailQuery(growthId);
+  const deleteMutation = useDeleteGrowthMutation();
+  const router = useRouter();
+  const [Modal, handleModal] = useModal();
+
+  const { category, nickname, writerProfileImageUrl, petProfileImageUrl, content, isMine, petName } =
+    growthDatas?.data as GrowthDetailsData;
+
+  const openModal = () => {
+    handleModal(true);
+  };
+  const closeModal = () => {
+    handleModal(false);
+    router.push('/growth');
+  };
+  const handleEditClick = () => {
+    router.push(`/growth/${growthId}/edit`);
+  };
+  const handleDeleteClick = () => {
+    deleteMutation.mutate(growthId, {
+      onSuccess: () => openModal(),
+    });
+  };
   return (
     <main className={styles.outer}>
       <div className={styles.inner}>
@@ -30,35 +58,33 @@ export default function GrowthDetailPage({ growthId }: GrowthDetailPageProps) {
             <div className={styles.tempImg}></div>
           </div>
           <div className={styles.headerText}>
-            <p className={styles.category}>{todo.category}</p>
-            <p>{todo.title}</p>
+            <p className={styles.category}>{category}</p>
             <div className={styles.profiles}>
-              <CalendarTodoProfile src={todo.writerProfileImageUrl} name={todo.writerNickName} />
-              <CalendarTodoProfile src={todo.petProfileImageUrl} name={todo.petName} />
+              <CalendarTodoProfile src={writerProfileImageUrl} name={nickname} />
+              <CalendarTodoProfile src={petProfileImageUrl} name={petName} />
             </div>
           </div>
-          <div className={styles.kebab}>
-            <DropdownMenu value={{ isOpen, onCloseToggle, onOpenToggle }}>
-              <DropdownMenu.Kebab />
-              <DropdownMenu.Content>
-                <DropdownMenu.Item onClick={handleClickEdit}>수정</DropdownMenu.Item>
-                <DropdownMenu.Item onClick={handleClickDelete}>삭제</DropdownMenu.Item>
-              </DropdownMenu.Content>
-            </DropdownMenu>
-          </div>
+          {isMine ? (
+            <div className={styles.kebab}>
+              <DropdownMenu value={{ isOpen, onCloseToggle, onOpenToggle }}>
+                <DropdownMenu.Kebab />
+                <DropdownMenu.Content>
+                  <DropdownMenu.Item onClick={handleEditClick}>수정</DropdownMenu.Item>
+                  <DropdownMenu.Item onClick={handleDeleteClick}>삭제</DropdownMenu.Item>
+                </DropdownMenu.Content>
+              </DropdownMenu>
+            </div>
+          ) : null}
         </div>
 
         <div className={styles.memoContainer}>
           <Image src="/images/calendar/calendar.svg" alt="달력 아이콘" width={20} height={20} />
-          <section className={styles.memo}>{todo.memo}</section>
-        </div>
-
-        <div className={styles.button}>
-          <Button onClick={handleClickFinished} round className={todo.isFinished ? 'disabled' : 'primary'}>
-            {todo.isFinished ? '완료 해제하기' : '완료로 표시하기'}
-          </Button>
+          <section className={styles.memo}>{content.memo}</section>
         </div>
       </div>
+      <Modal>
+        <CompleteModal closeModal={closeModal} text="성장 기록이 삭제되었습니다." />
+      </Modal>
     </main>
   );
 }
