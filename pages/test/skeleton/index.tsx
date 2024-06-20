f0791a99cc01c1f251851a3e29267a77d977008d
mun-jihye <m84422583@gmail.com>
2024-06-21 01:23:52 +0900

    [#145] feat: family growth skeleton 구현

--- pages/test/skeleton/index.tsx
+++ pages/test/skeleton/index.tsx
@@ -1,10 +1,12 @@
 import FamilyDiarySkeleton from '@/components/skeleton/family/diary';
+import FamilyGrowthSkeleton from '@/components/skeleton/family/growth';
 import React from 'react';
 
 export default function index() {
   return (
     <div style={{ paddingTop: '54px' }}>
-      <FamilyDiarySkeleton />
+      {/* <FamilyDiarySkeleton /> */}
+      <FamilyGrowthSkeleton />
     </div>
   );
 }
