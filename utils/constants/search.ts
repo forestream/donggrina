63de7622c568a6cce8bd785601a2997783309c2c
hanbin <rj2303@naver.com>
2024-06-20 23:23:35 +0900

    [#115] Feat: 필터 인풋 값에 영문 할당

--- utils/constants/search.ts
+++ utils/constants/search.ts
@@ -5,16 +5,19 @@ export const FILTERS = [
     imageOn: '/images/search/calendar-on.svg',
     imageOff: '/images/search/calendar-off.svg',
     name: '일정기록',
+    value: 'calendar',
   },
   {
     imageOn: '/images/search/diary-on.svg',
     imageOff: '/images/search/diary-off.svg',
     name: '다이어리',
+    value: 'diary',
   },
   {
     imageOn: '/images/search/growth-on.svg',
     imageOff: '/images/search/growth-off.svg',
     name: '성장기록',
+    value: 'growth',
   },
 ];
 
