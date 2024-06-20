148b3ba1f39e409026a78f74ee83d53eb04062a3
hanbin <rj2303@naver.com>
2024-06-20 12:28:43 +0900

    [#115] Fix: 날짜 변환 메서드 에러 해결
    단축 평가 사용

--- pages/calendar/[calendarId]/index.tsx
+++ pages/calendar/[calendarId]/index.tsx
@@ -64,7 +64,7 @@ export default function CalendarById({ calendarId }: InferGetServerSidePropsType
               </span>
               <span className={styles.dot}></span>
               <span>
-                {ampm} {hour.toString().padStart(2, '0')}:{minute.toString().padStart(2, '0')}
+                {ampm} {hour && hour.toString().padStart(2, '0')}:{minute && minute.toString().padStart(2, '0')}
               </span>
             </p>
             <div className={styles.profiles}>
