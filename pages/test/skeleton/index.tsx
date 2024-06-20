ba1c68eea45d8ad9a87006ba8485630154a214ca
mun-jihye <m84422583@gmail.com>
2024-06-21 01:15:46 +0900

    [#145] feat: family diary skeleton 구현

---
+++ pages/test/skeleton/index.tsx
@@ -0,0 +1,10 @@
+import FamilyDiarySkeleton from '@/components/skeleton/family/diary';
+import React from 'react';
+
+export default function index() {
+  return (
+    <div style={{ paddingTop: '54px' }}>
+      <FamilyDiarySkeleton />
+    </div>
+  );
+}
