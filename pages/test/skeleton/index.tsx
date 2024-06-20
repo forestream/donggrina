b3be7f729c89fe4d9882604da29541b899f2c699
mun-jihye <m84422583@gmail.com>
2024-06-21 00:27:24 +0900

    [#145] feat: growth list ui 구현 완료

---
+++ pages/test/skeleton/index.tsx
@@ -0,0 +1,10 @@
+import GrowthListSkeleton from '@/components/skeleton/growth/growth-list';
+import React from 'react';
+
+export default function Test() {
+  return (
+    <div style={{ paddingTop: '60px' }}>
+      <GrowthListSkeleton />
+    </div>
+  );
+}
