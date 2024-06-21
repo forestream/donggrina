6d54fbb13ac3da9396c8958e32e86461a68ca140
mun-jihye <m84422583@gmail.com>
2024-06-21 17:37:28 +0900

    [#145] feat: 패밀리 스켈레톤 적용

--- components/family/growth/family-growth.tsx
+++ components/family/growth/family-growth.tsx
@@ -1,8 +1,20 @@
+import FamilyGrowthSkeleton from '@/components/skeleton/family/growth';
 import styles from './family-growth.module.scss';
 import FamilyGrowthContents from '@/components/family/growth/family-growth-contents';
 import Suspensive from '@/components/suspensive/suspensive';
 import { useFetchGrowth } from '@/hooks/queries/family';
 import CalendarInstance from '@/utils/date/date.utils';
+
+const FamilyGrowthSkeletonContainer = () => {
+  return (
+    <div className={styles.skeletonContainer}>
+      <FamilyGrowthSkeleton />
+      <FamilyGrowthSkeleton />
+      <FamilyGrowthSkeleton />
+      <FamilyGrowthSkeleton />
+    </div>
+  );
+};
 
 export default function FamilyGrowth() {
   const date = CalendarInstance.getTodayData();
@@ -11,7 +23,7 @@ export default function FamilyGrowth() {
   return (
     <section className={styles.wrapper}>
       <h2 className={styles.title}>성장기록</h2>
-      <Suspensive isLoading={growthQuery.isLoading}>
+      <Suspensive fallback={<FamilyGrowthSkeletonContainer />} isLoading={growthQuery.isLoading}>
         <FamilyGrowthContents />
       </Suspensive>
     </section>
