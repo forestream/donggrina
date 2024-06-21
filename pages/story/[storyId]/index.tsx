8535629c2f04612eb57c932d5e39a49fb49c6b4c
LimDong <ilhai6@naver.com>
2024-06-21 22:03:57 +0900

    [#145] Feat: 스토리 상세 스켈레톤 작업중

--- pages/story/[storyId]/index.tsx
+++ pages/story/[storyId]/index.tsx
@@ -1,6 +1,7 @@
 import StoryDetailHeader from '@/components/story/detail/story-detail-header';
 import StoryDetailPost from '@/components/story/detail/story-detail-post';
 import Suspensive from '@/components/suspensive/suspensive';
+import StoryListItemSkeleton from '@/components/skeleton/story/item/story-list-item-skeleton';
 
 import { useFetchDetailStory } from '@/hooks/queries/story';
 import { useRouter } from 'next/router';
@@ -14,7 +15,7 @@ export default function StoryDetailPage() {
   return (
     <div style={{ padding: '54px 24px 0px 24px' }}>
       <StoryDetailHeader isMyStory={detailQuery.data?.isMyStory} />
-      <Suspensive isLoading={detailQuery.isLoading}>
+      <Suspensive isLoading={detailQuery.isLoading} fallback={<StoryListItemSkeleton />}>
         <StoryDetailPost />
       </Suspensive>
     </div>
