669c645e4729d21be9af3b7d64dd047ebd9d71d4
LimDong <ilhai6@naver.com>
2024-06-21 23:23:36 +0900

    [#145] Feat: 스피너 작업 완료

--- utils/is-nav.ts
+++ utils/is-nav.ts
@@ -2,6 +2,5 @@ import { NextRouter } from 'next/router';
 
 export const isNav = (router: NextRouter) => {
   const navPaths = ['/calendar', '/family', '/diaries', '/growth', '/story'];
-  console.log(navPaths.some((path) => router.pathname === path));
   return navPaths.some((path) => router.pathname === path);
 };
