b913503e2cbd8f19a990d67174a8ee5e896b7988
hanbin <rj2303@naver.com>
2024-06-22 09:26:15 +0900

    [#65] Feat: 다이어리 항목 클릭 시 상세로 이동 동작 구현

--- components/diaries/diary-content/diary-content.tsx
+++ components/diaries/diary-content/diary-content.tsx
@@ -5,6 +5,7 @@ import Response from '../diary-content/response';
 import Kebab from '../diary-content/kebab';
 import ContentImage from '../diary-content/content-image';
 import { useDiaries } from '@/hooks/queries/diary/use-diary-query';
+import { useRouter } from 'next/router';
 
 interface DiaryContentProps {
   date: string;
@@ -13,29 +14,35 @@ interface DiaryContentProps {
 const DiaryContent: React.FC<DiaryContentProps> = ({ date }) => {
   const { data, isError, isLoading } = useDiaries(date);
 
+  const router = useRouter();
+
   if (isLoading) return <p>Loading...</p>;
   if (!data || data.length === 0) return <></>;
   if (isError) return <p>error</p>;
 
   return (
     <div>
-      {data?.map((diary) => (
-        <div key={diary.diaryId} className={styles.diaryContent}>
-          <div className={styles.leftContainer}>
-            <Profile author={diary.author} authorImage={diary.authorImage} petImages={diary.petImages} />
-            <Content content={diary.content} />
-            <Response
-              commentCount={diary.commentCount}
-              favoriteCount={diary.favoriteCount}
-              favoriteState={diary.favoriteState}
-            />
+      {data?.map((diary) => {
+        const handleClick = () => router.push(`/diaries/${diary.diaryId}`);
+
+        return (
+          <div key={diary.diaryId} className={styles.diaryContent} onClick={handleClick}>
+            <div className={styles.leftContainer}>
+              <Profile author={diary.author} authorImage={diary.authorImage} petImages={diary.petImages} />
+              <Content content={diary.content} />
+              <Response
+                commentCount={diary.commentCount}
+                favoriteCount={diary.favoriteCount}
+                favoriteState={diary.favoriteState}
+              />
+            </div>
+            <div className={styles.rightContainer}>
+              {diary.isMyDiary && <Kebab diaryId={diary.diaryId} />}
+              <ContentImage contentImage={diary.contentImage} />
+            </div>
           </div>
-          <div className={styles.rightContainer}>
-            {diary.isMyDiary && <Kebab diaryId={diary.diaryId} />}
-            <ContentImage contentImage={diary.contentImage} />
-          </div>
-        </div>
-      ))}
+        );
+      })}
     </div>
   );
 };
