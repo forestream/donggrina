35bdac20531ed0c71218eac448eece9076962844
mun-jihye <m84422583@gmail.com>
2024-06-23 14:16:26 +0900

    [#146] fix: 마이페이지 이동 에러 수정

--- components/common/Header/main-header/index.tsx
+++ components/common/Header/main-header/index.tsx
@@ -7,7 +7,7 @@ export default function MainHeader() {
   const router = useRouter();
 
   const handleClick = () => {
-    router.back();
+    router.push('/mypage');
   };
 
   const handleSearchClick = () => {
