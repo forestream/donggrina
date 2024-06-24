7a81002b477afaa75221b8ab5e04968311e989e2
LimDong <ilhai6@naver.com>
2024-06-24 19:55:03 +0900

    [#185] Feat: 여백 공간 퍼블리싱 완료

--- components/background/background.tsx
+++ components/background/background.tsx
@@ -1,24 +1,42 @@
 import { PropsWithChildren } from 'react';
 import styles from './background.module.scss';
 import LogoSVG from '@/public/images/header/logo.svg';
+import { motion } from 'framer-motion';
+import { horizontalVariants, textVariants } from '../framer';
+
+interface CircleClassType {
+  className: 'circle' | 'smallCircle';
+}
+function Circle({ className }: CircleClassType) {
+  return <div className={styles[className]}></div>;
+}
 
 export default function BackGround({ children }: PropsWithChildren) {
   return (
     <article className={styles.article}>
       <section className={styles.section}>
-        <header aria-label="동그리나 로고">
+        <motion.header aria-label="동그리나 로고" variants={textVariants} initial="hidden" animate="visible" custom={0}>
           <LogoSVG />
-        </header>
-        <div>
-          <h2>
+        </motion.header>
+        <div className={styles.titleBox}>
+          <motion.h2 variants={textVariants} initial="hidden" animate="visible" custom={1}>
             반려동물 <strong>1등</strong>다이어리
             <br />
             <strong>동그리나</strong>
-          </h2>
-          <p>
+          </motion.h2>
+          <motion.p variants={textVariants} initial="hidden" animate="visible" custom={2}>
             동그리나는 가족, 친구,연인과 함께 사용할 수 있는 플랫폼이에요.
             <br /> 동그리나에서 같이 사용할 가족을 만들어보세요.
-          </p>
+          </motion.p>
+        </div>
+        <div className={styles.circleBox1}>
+          <Circle className="circle" />
+        </div>
+        <div className={styles.circleBox2}>
+          <Circle className="circle" />
+        </div>
+        <div className={styles.circleBox3}>
+          <Circle className="smallCircle" />
         </div>
       </section>
       {children}
