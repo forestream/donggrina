0940c543f361e0c63e671760ad0a65cdbfba1f7c
hanbin <rj2303@naver.com>
2024-07-16 23:34:05 +0900

    nav 미사용 버튼 제거.
    헤더 미사용 버튼 페이지 이동 기능 제거

--- components/family/growth/item/family-growth-item.tsx
+++ components/family/growth/item/family-growth-item.tsx
@@ -1,4 +1,4 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 import { AvatarImage } from '@/components/avatar/avatar';
 import { GrowthData } from '@/types/growth';
 import { GROWTH_CATEGORY_FAMILY_ICON } from '@/utils/constants/growth';
@@ -7,12 +7,13 @@ import styles from './family-growth-item.module.scss';
 
 export default function FamilyGrowthItem(props: GrowthData) {
   return (
-    <Link href={`/growth/${props.id}`} className={styles['growth-item__layout']}>
+    // <Link href={`/growth/${props.id}`} className={styles['growth-item__layout']}>
+    <div className={styles['growth-item__layout']}>
       <div className={styles['growth-item__type']}>
         <Image src={GROWTH_CATEGORY_FAMILY_ICON[props.category]} alt={props.category} width={20} height={20} />
       </div>
       <AvatarImage border="main" image={props.writerProfileImageUrl} />
       <AvatarImage border="main" image={props.petProfileImageUrl} />
-    </Link>
+    </div>
   );
 }
--- utils/constants/nav-data.ts
+++ utils/constants/nav-data.ts
@@ -1,8 +1,8 @@
 import BookSVG from '@/public/images/nav/book.svg';
 import CalendarSVG from '@/public/images/nav/calendar.svg';
-import HomeSVG from '@/public/images/nav/home.svg';
-import SmileSVG from '@/public/images/nav/smile.svg';
-import GrowthSVG from '@/public/images/nav/growth.svg';
+// import HomeSVG from '@/public/images/nav/home.svg';
+// import SmileSVG from '@/public/images/nav/smile.svg';
+// import GrowthSVG from '@/public/images/nav/growth.svg';
 
 export const NAV_LIST_DATA = [
   {
@@ -15,19 +15,19 @@ export const NAV_LIST_DATA = [
     RenderSvgComponent: CalendarSVG,
     href: '/calendar',
   },
-  {
-    text: '다이어리',
-    RenderSvgComponent: HomeSVG,
-    href: '/diaries',
-  },
-  {
-    text: '성장기록',
-    RenderSvgComponent: GrowthSVG,
-    href: '/growth',
-  },
-  {
-    text: '스토리',
-    RenderSvgComponent: SmileSVG,
-    href: '/story',
-  },
+  // {
+  //   text: '다이어리',
+  //   RenderSvgComponent: HomeSVG,
+  //   href: '/diaries',
+  // },
+  // {
+  //   text: '성장기록',
+  //   RenderSvgComponent: GrowthSVG,
+  //   href: '/growth',
+  // },
+  // {
+  //   text: '스토리',
+  //   RenderSvgComponent: SmileSVG,
+  //   href: '/story',
+  // },
 ];
